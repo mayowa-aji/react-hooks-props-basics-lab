b53c8721e64e9f0d458d2ab8cdc5db393df9a09b
Mayowa Ajiboye <64916584+mayowa-aji@users.noreply.github.com>
2023-07-26 22:01:22 -0400

    solution

---
+++ src/components/Links.js
@@ -0,0 +1,13 @@
+function Links({github,linkedin }) {
+  return (
+    <>
+      <h3>Links</h3>
+      <a href={github}>{github}</a>
+      <a href={linkedin}>{linkedin}</a>
+
+    </>
+
+  );
+}
+
+export default Links
